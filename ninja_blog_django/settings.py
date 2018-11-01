346b187c40ef894d8a10bc4c8008c98c176ee016
markwilde33 <markwilde29@gmail.com>
2018-11-01 18:11:45 +0000

    configured heroku postgres database and used it as new database in settings.py

--- ninja_blog_django/settings.py
+++ ninja_blog_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,13 +74,15 @@ WSGI_APPLICATION = 'ninja_blog_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {'default': dj_database_url.parse(
+    "postgres://mvqgivovabrnei:c021e977ede4da05e2294904d26ef6c1b0abb41f40688774b5000a2824fe463c@ec2-54-247-98-162.eu-west-1.compute.amazonaws.com:5432/d9jgtofmkb59p4")}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
